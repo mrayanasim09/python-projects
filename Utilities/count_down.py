9f0d3059ec03a38398f2b784700fc86c45a0bf21
MRayan Asim <112205950+drik493@users.noreply.github.com>
2023-07-05 23:46:54 +0500

    Update count_down.py

--- Utilities/count_down.py
+++ Utilities/count_down.py
@@ -36,37 +36,6 @@ def countdown_timer(seconds):
     pygame.mixer.music.stop()
     pygame.mixer.quit()
 
-    save_feedback_with_name()
-    display_previous_feedbacks()
-
-    suggestion_input = input("Would you like to provide any suggestions? (yes/no): ")
-    if suggestion_input.lower() == "yes":
-        suggestion_text = input("Enter your suggestions: ")
-        with open(new_filename, "a") as suggestion_file:
-            suggestion_file.write(suggestion_text + ' :count_down \n')
-
-    print("Ok, hope you liked our count down timer. Goodbye! MRayanasim")
-    text = "MRayanasim made this count down timer thanks for using"
-    formatted_text = "** " + text.upper() + " **"
-    print("*" * len(formatted_text))
-    print(formatted_text)
-    print("*" * len(formatted_text))
-    quit()
-
-def save_feedback_with_name():
-    name = input("Enter your name: ")
-    feedback = input("Please provide your feedback: ")
-    with open("feedback.txt", "a") as file:
-        file.write(name + ": " + feedback +' :countdown_timer'+ "\n")
-
-def display_previous_feedbacks():
-    print("Previous Feedbacks:")
-    with open("feedback.txt", "r") as file:
-        for line in file:
-            print(line.strip())
-
-filename = "feedback.txt"
-new_filename = "suggestion.txt"
 
 # Wait for user to enter "start"
 start_input = input("Enter 'start' to begin the countdown: ")
