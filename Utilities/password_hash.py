6d37f3e56000b17e62c5f70dc6436426ae37ce14
MRayan Asim <112205950+drik493@users.noreply.github.com>
2023-07-05 23:03:29 +0500

    Update password_hash.py

--- Utilities/password_hash.py
+++ Utilities/password_hash.py
@@ -1,1 +1,19 @@
+#This code is made by MRayan Asim
+#Packages needed:
+#pip install hashlib
+# Python 3 code to demonstrate the
+# working of MD5 (string - hexadecimal)
+
+import hashlib
+
+# initializing string
+str2hash = input("")
+
+#e
+# then sending to md5()
+result = hashlib.md5(str2hash.encode())
+
+# printing the equivalent hexadecimal value.
+print("The hexadecimal equivalent of hash is : ", end ="")
+print(result.hexdigest())
 
