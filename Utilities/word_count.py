d33ca548e155ead2cb0b3a831ea91547a9f50799
MRayan Asim <112205950+mrayanasim09@users.noreply.github.com>
2023-07-14 15:04:46 +0500

    Create word_count.py

---
+++ Utilities/word_count.py
@@ -0,0 +1,26 @@
+#This code is made by MRayan Asim
+import string
+sentence = input("Enter the text: ")
+def count_letters_in_sentence(sentence):
+    # Remove any punctuation marks and split the sentence into words
+    words = sentence.replace(",", "").replace(".", "").split()
+    
+    # Initialize the letter count
+    count = 0
+    
+    # Iterate over each word and count the letters
+    for word in words:
+        count += len(word)
+    
+    # Return the total count of letters
+    return count
+
+# printing original string
+print ("The original string is: " + sentence)
+# using sum() + strip() + split() function
+res = sum([i.strip(string.punctuation).isalpha() for i in
+sentence.split()])
+# no of words
+letter_count = count_letters_in_sentence(sentence)
+print("Number of letters:", letter_count)
+print ("The number of words in string are : " + str(res))
