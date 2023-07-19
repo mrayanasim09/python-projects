a178973ee1dc2ac3193227d71609ebf992ad1b8d
MRayan Asim <112205950+mrayanasim09@users.noreply.github.com>
2023-07-19 13:19:17 +0500

    Create text_to_speech.py

---
+++ Utilities/text_to_speech.py
@@ -0,0 +1,25 @@
+#This code is made by MRayan Asim
+#Packages needed:
+#pip install pyttsx3
+import pyttsx3
+
+def onStart():
+    print('starting')
+
+def onWord(name, location, length):
+    print('word', name, location, length)
+
+def onEnd(name, completed):
+    print('finishing', name, completed)
+
+engine = pyttsx3.init()
+
+engine.connect('started-utterance', onStart)
+engine.connect('started-word', onWord)
+engine.connect('finished-utterance', onEnd)
+
+sen = input("Enter the text: ")
+
+
+engine.say(sen)
+engine.runAndWait()
