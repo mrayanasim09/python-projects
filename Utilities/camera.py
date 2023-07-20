af34eeedd846d06012f4654c56d85e019a9ab390
MRayan Asim <112205950+mrayanasim09@users.noreply.github.com>
2023-07-20 22:10:43 +0500

    Add files via upload

---
+++ Utilities/camera.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+ip_address = input("Enter the ip address: ")
+port = input("Enter the port number: ")
+# Replace this with the IP address and port number from the camera app
+camera_url = f'http://{ip_address}:{port}/video'
+
+# Create VideoCapture object to read from the camera stream
+cap = cv2.VideoCapture(camera_url)
+
+while True:
+    # Read frame-by-frame from the camera
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Perform image processing or display the frame
+    # Example: Convert to grayscale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the frame
+    cv2.imshow('Camera Feed', gray_frame)
+
+    # Exit on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the VideoCapture and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
