f05dbdf52d8c9af4fa7cf410eb8beac2cf83518d
MRayan Asim <112205950+mrayanasim09@users.noreply.github.com>
2023-07-26 02:46:21 +0500

    Update word_count.py

--- Utilities/word_count.py
+++ Utilities/word_count.py
@@ -1,6 +1,8 @@
-#This code is made by MRayan Asim
+# This code is made by MRayan Asim
 import string
+
 sentence = input("Enter the text: ")
+
 def count_letters_in_sentence(sentence):
     # Remove any punctuation marks and split the sentence into words
     words = sentence.replace(",", "").replace(".", "").split()
@@ -15,12 +17,15 @@ def count_letters_in_sentence(sentence):
     # Return the total count of letters
     return count
 
-# printing original string
-print ("The original string is: " + sentence)
-# using sum() + strip() + split() function
-res = sum([i.strip(string.punctuation).isalpha() for i in
-sentence.split()])
-# no of words
+
+
+# Printing original string
+print("The original string is: " + sentence)
+
+# Using sum() + strip() + split() function
+res = sum([i.strip(string.punctuation).isalpha() for i in sentence.split()])
+
+# Number of words
 letter_count = count_letters_in_sentence(sentence)
 print("Number of letters:", letter_count)
-print ("The number of words in string are : " + str(res))
+print("The number of words in the string is:", str(res))
