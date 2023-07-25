982bc10ee46a4161a67a5a5312544d14284625d2
MRayan Asim <112205950+mrayanasim09@users.noreply.github.com>
2023-07-26 03:46:20 +0500

    Update graphics.py

--- Utilities/graphics.py
+++ Utilities/graphics.py
@@ -1,280 +1,77 @@
-#This code is made by MRayan Asim
+# This code is made by MRayan Asim
 import turtle as tu
 
-roo = tu.Turtle()  # Turtle object
-wn = tu.Screen()  # Screen Object
-wn.bgcolor("black")  # Screen Bg color
-wn.title("Fractal Tree Pattern")
-roo.left(90)  # moving the turtle 90 degrees towards left
-roo.speed(20)  # setting the speed of the turtle
+# Create a turtle object
+fractal_turtle = tu.Turtle()
 
+# Set up the screen
+screen = tu.Screen()
+screen.bgcolor("black")  # Screen Bg color
+screen.title("Fractal Tree Pattern")
 
-def draw(l):  # recursive function taking length 'l' as argument
-    if (l < 10):
+fractal_turtle.left(90)  # moving the turtle 90 degrees towards the left
+fractal_turtle.speed(20)  # setting the speed of the turtle
+
+# Recursive function to draw the fractal tree
+def draw_fractal_tree(length):
+    if (length < 10):
         return
     else:
-
-        roo.pensize(2)  # Setting Pensize
-        roo.pencolor("yellow")  # Setting Pencolor as yellow
-        roo.forward(l)  # moving turtle forward by 'l'
-        roo.left(30)  # moving the turtle 30 degrees towards left
-        draw(3 * l / 4)  # drawing a fractal on the left of the turtle object 'roo' with 3/4th of its length
-        roo.right(60)  # moving the turtle 60 degrees towards right
-        draw(3 * l / 4)  # drawing a fractal on the right of the turtle object 'roo' with 3/4th of its length
-        roo.left(30)  # moving the turtle 30 degrees towards left
-        roo.pensize(2)
-        roo.backward(l)  # returning the turtle back to its original psition
-
-
-draw(20)  # drawing 20 times
-
-roo.right(90)
-roo.speed(2000)
+        fractal_turtle.pensize(2)  # Setting Pensize
+        fractal_turtle.pencolor("yellow")  # Setting Pencolor as yellow
+        fractal_turtle.forward(length)  # moving the turtle forward by 'length'
+        fractal_turtle.left(30)  # moving the turtle 30 degrees towards the left
+        draw_fractal_tree(3 * length / 4)  # drawing a fractal on the left with 3/4th of its length
+        fractal_turtle.right(60)  # moving the turtle 60 degrees towards the right
+        draw_fractal_tree(3 * length / 4)  # drawing a fractal on the right with 3/4th of its length
+        fractal_turtle.left(30)  # moving the turtle 30 degrees towards the left
+        fractal_turtle.pensize(2)
+        fractal_turtle.backward(length)  # returning the turtle back to its original position
 
 
-# recursion
-def draw(l):
-    if (l < 10):
+draw_fractal_tree(20)  # drawing the fractal tree 20 times
+
+fractal_turtle.right(90)
+fractal_turtle.speed(2000)
+
+# Recursive function to draw the fractal tree
+def draw_fractal_tree(length):
+    if (length < 10):
         return
     else:
-        roo.pensize(2)
-        roo.pencolor("magenta")  # magenta
-        roo.forward(l)
-        roo.left(30)
-        draw(3 * l / 4)
-        roo.right(60)
-        draw(3 * l / 4)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
+        fractal_turtle.pensize(2)
+        fractal_turtle.pencolor("magenta")  # magenta
+        fractal_turtle.forward(length)
+        fractal_turtle.left(30)
+        draw_fractal_tree(3 * length / 4)
+        fractal_turtle.right(60)
+        draw_fractal_tree(3 * length / 4)
+        fractal_turtle.left(30)
+        fractal_turtle.pensize(2)
+        fractal_turtle.backward(length)
 
 
-draw(20)
+draw_fractal_tree(20)
 
-roo.left(270)
-roo.speed(2000)
+fractal_turtle.left(270)
+fractal_turtle.speed(2000)
 
-
-# recursion
-def draw(l):
-    if (l < 10):
+# Recursive function to draw the fractal tree
+def draw_fractal_tree(length):
+    if (length < 10):
         return
     else:
-        roo.pensize(2)
-        roo.pencolor("red")  # red
-        roo.forward(l)
-        roo.left(30)
-        draw(3 * l / 4)
-        roo.right(60)
-        draw(3 * l / 4)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
+        fractal_turtle.pensize(2)
+        fractal_turtle.pencolor("red")  # red
+        fractal_turtle.forward(length)
+        fractal_turtle.left(30)
+        draw_fractal_tree(3 * length / 4)
+        fractal_turtle.right(60)
+        draw_fractal_tree(3 * length / 4)
+        fractal_turtle.left(30)
+        fractal_turtle.pensize(2)
+        fractal_turtle.backward(length)
 
 
-draw(20)
+draw_fractal_tree(20)
 
-roo.right(90)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(2)
-        roo.pencolor('#FFF8DC')  # white
-        roo.forward(l)
-        roo.left(30)
-        draw(3 * l / 4)
-        roo.right(60)
-        draw(3 * l / 4)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
-
-
-draw(20)
-
-
-########################################################
-
-def draw(l):
-    if (l < 10):
-        return
-    else:
-
-        roo.pensize(3)
-        roo.pencolor("lightgreen")  # lightgreen
-        roo.forward(l)
-        roo.left(30)
-        draw(4 * l / 5)
-        roo.right(60)
-        draw(4 * l / 5)
-        roo.left(30)
-        roo.pensize(3)
-        roo.backward(l)
-
-
-draw(40)
-
-roo.right(90)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(3)
-        roo.pencolor("red")  # red
-        roo.forward(l)
-        roo.left(30)
-        draw(4 * l / 5)
-        roo.right(60)
-        draw(4 * l / 5)
-        roo.left(30)
-        roo.pensize(3)
-        roo.backward(l)
-
-
-draw(40)
-
-roo.left(270)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(3)
-        roo.pencolor("yellow")  # yellow
-        roo.forward(l)
-        roo.left(30)
-        draw(4 * l / 5)
-        roo.right(60)
-        draw(4 * l / 5)
-        roo.left(30)
-        roo.pensize(3)
-        roo.backward(l)
-
-
-draw(40)
-
-roo.right(90)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(3)
-        roo.pencolor('#FFF8DC')  # white
-        roo.forward(l)
-        roo.left(30)
-        draw(4 * l / 5)
-        roo.right(60)
-        draw(4 * l / 5)
-        roo.left(30)
-        roo.pensize(3)
-        roo.backward(l)
-
-
-draw(40)
-
-
-########################################################
-def draw(l):
-    if (l < 10):
-        return
-    else:
-
-        roo.pensize(2)
-        roo.pencolor("cyan")  # cyan
-        roo.forward(l)
-        roo.left(30)
-        draw(6 * l / 7)
-        roo.right(60)
-        draw(6 * l / 7)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
-
-
-draw(60)
-
-roo.right(90)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(2)
-        roo.pencolor("yellow")  # yellow
-        roo.forward(l)
-        roo.left(30)
-        draw(6 * l / 7)
-        roo.right(60)
-        draw(6 * l / 7)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
-
-
-draw(60)
-
-roo.left(270)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(2)
-        roo.pencolor("magenta")  # magenta
-        roo.forward(l)
-        roo.left(30)
-        draw(6 * l / 7)
-        roo.right(60)
-        draw(6 * l / 7)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
-
-
-draw(60)
-
-roo.right(90)
-roo.speed(2000)
-
-
-# recursion
-def draw(l):
-    if (l < 10):
-        return
-    else:
-        roo.pensize(2)
-        roo.pencolor('#FFF8DC')  # white
-        roo.forward(l)
-        roo.left(30)
-        draw(6 * l / 7)
-        roo.right(60)
-        draw(6 * l / 7)
-        roo.left(30)
-        roo.pensize(2)
-        roo.backward(l)
-
-
-draw(60)
-wn.exitonclick()
