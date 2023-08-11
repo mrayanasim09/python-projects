9533fcecad4cfee41285e1132b51a5006f516104
mrayanasim09 <mrayanasim09@users.noreply.github.com>
2023-08-11 20:12:46 +0000

    :art: Format Python code with psf/black

--- Game/__init__.py
+++ Game/__init__.py
@@ -5,10 +5,10 @@ import dice.py
 import hangman.py
 import master_mid.py
 import number_details.py
-import number_guessing.py 
+import number_guessing.py
 import rock_paper_scissors.py
 import snake_game.py
-import snake_ladder.py 
+import snake_ladder.py
 import star.py
 import tick_cross.py
 import tick_cross_gui.py
