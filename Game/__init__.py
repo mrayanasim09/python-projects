e5ea372e7f3a8467bdaea5fa255935e8d78d0275
MRayan Asim <mrayanasim09@gmail.com>
2023-08-12 01:11:26 +0500

    Update __init__.py
    Signed-off-by: MRayan Asim <mrayanasim09@gmail.com>

--- Game/__init__.py
+++ Game/__init__.py
@@ -6,11 +6,11 @@ import hangman.py
 import master_mid.py
 import number_details.py
 import number_guessing.py 
-import rock-paper-scissors.py
+import rock_paper_scissors.py
 import snake_game.py
 import snake_ladder.py 
 import star.py
 import tick_cross.py
 import tick_cross_gui.py
-import twenty-one.py
+import twenty_one.py
 import typing_speed.py
