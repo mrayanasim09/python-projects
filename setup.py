e22f5393d4794a78573c9d855480dbdd75fd2da9
MRayan Asim <mrayanasim09@gmail.com>
2023-08-11 23:58:41 +0500

    changed the package name
    Signed-off-by: MRayan Asim <mrayanasim09@gmail.com>

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ import atexit
 atexit.register(print_install_message)
 
 setup(
-    name="100-python-projects",
-    version="0.2",
+    name="100_python_projects",
+    version="0.5",
     description="A collection of Python projects",
     author="MRayan Asim",
     author_email="mrayanasim09@gmail.com",
