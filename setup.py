8affc2e84a227255f312775388c73475144d946a
mrayanasim09 <mrayanasim09@users.noreply.github.com>
2023-08-11 14:08:45 +0000

    :art: Format Python code with psf/black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-projects',  # This is the package name users will use for installation
-    version='0.1',
-    description='A collection of Python projects',
-    author='Your Name',
-    author_email='mrayanasim09@gmail.com',
+    name="python-projects",  # This is the package name users will use for installation
+    version="0.1",
+    description="A collection of Python projects",
+    author="Your Name",
+    author_email="mrayanasim09@gmail.com",
     packages=find_packages(),
 )
