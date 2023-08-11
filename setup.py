37a5477463d879d745b9807ca8d67101bbf3e9d7
MRayan Asim <mrayanasim09@gmail.com>
2023-08-11 19:01:55 +0500

    added for the package
    Signed-off-by: MRayan Asim <mrayanasim09@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='python-projects',
+    version='0.1',
+    description='A collection of Python projects',
+    author='MRayan Asim',
+    author_email='mrayanasim09@gmail.com',
+    packages=['python_projects'],
+)
