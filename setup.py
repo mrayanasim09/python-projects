342c060b063b5d6b400050e4e1005d39f557aeba
MRayan Asim <mrayanasim09@gmail.com>
2023-08-12 01:37:08 +0500

    Update setup.py
    Signed-off-by: MRayan Asim <mrayanasim09@gmail.com>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import atexit
 atexit.register(print_install_message)
 
 setup(
-    name="python__projects",
+    name="top_python_projects",
     version="0.5",
     description="A collection of Python projects",
     author="MRayan Asim",
