f3f15339d0910c7ef61cd3df729886fb7e218564
MRayan Asim <mrayanasim09@gmail.com>
2023-08-12 01:09:27 +0500

    made the __intit__.py for package
    Signed-off-by: MRayan Asim <mrayanasim09@gmail.com>

---
+++ Game/__init__.py
@@ -0,0 +1,16 @@
+import blocks.py
+import color_guessing.py
+import colox.py
+import dice.py
+import hangman.py
+import master_mid.py
+import number_details.py
+import number_guessing.py 
+import rock-paper-scissors.py
+import snake_game.py
+import snake_ladder.py 
+import star.py
+import tick_cross.py
+import tick_cross_gui.py
+import twenty-one.py
+import typing_speed.py
